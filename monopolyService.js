0ea0a179743ad4fd9ad59d9a3023d7598c9db3a9
jrmuys <jrmuyskens@gmail.com>
2020-11-03 16:34:47 -0500

    changed for homework 3

--- monopolyService.js
+++ monopolyService.js
@@ -12,11 +12,11 @@
 
 const pgp = require('pg-promise')();
 const db = pgp({
-    host: "salt.db.elephantsql.com",
-    port: 5432,
-    database: process.env.USER,
-    user: process.env.USER,
-    password: process.env.PASSWORD
+  host: 'salt.db.elephantsql.com',
+  port: 5432,
+  database: process.env.USER,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
 });
 
 // Configure the server and its routes.
@@ -27,12 +27,13 @@ const port = process.env.PORT || 3000;
 const router = express.Router();
 router.use(express.json());
 
-router.get("/", readHelloMessage);
-router.get("/players", readPlayers);
-router.get("/players/:id", readPlayer);
-router.put("/players/:id", updatePlayer);
+router.get('/', readHelloMessage);
+router.get('/players', readPlayers);
+router.get('/players/:id', readPlayer);
+router.put('/players/:id', updatePlayer);
 router.post('/players', createPlayer);
 router.delete('/players/:id', deletePlayer);
+router.get('/winners', readWinners);
 
 app.use(router);
 app.use(errorHandler);
@@ -41,70 +42,93 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 // Implement the CRUD operations.
 
 function errorHandler(err, req, res) {
-    if (app.get('env') === "development") {
-        console.log(err);
-    }
-    res.sendStatus(err.status || 500);
+  if (app.get('env') === 'development') {
+    console.log(err);
+  }
+  res.sendStatus(err.status || 500);
 }
 
 function returnDataOr404(res, data) {
-    if (data == null) {
-        res.sendStatus(404);
-    } else {
-        res.send(data);
-    }
+  if (data == null) {
+    res.sendStatus(404);
+  } else {
+    res.send(data);
+  }
+}
+
+function readWinners(req, res, next) {
+  db.many(
+    `SELECT name, score, email
+            FROM Player, Game, PlayerGame
+            WHERE Player.ID = PlayerGame.PlayerID
+            AND Game.ID = PlayerGame.GameID
+            ORDER BY score DESC
+            LIMIT 1`
+  )
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
 function readHelloMessage(req, res) {
-    res.send('This is Joels monopoly serivice, enjoy!');
+  res.send('This is Joels monopoly serivice, enjoy!');
 }
 
 function readPlayers(req, res, next) {
-    db.many("SELECT * FROM Player")
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            next(err);
-        })
+  db.many('SELECT * FROM Player')
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
 function readPlayer(req, res, next) {
-    db.oneOrNone(`SELECT * FROM Player WHERE id=${req.params.id}`)
-        .then(data => {
-            returnDataOr404(res, data);
-        })
-        .catch(err => {
-            next(err);
-        });
+  db.oneOrNone(`SELECT * FROM Player WHERE id=${req.params.id}`)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
 function updatePlayer(req, res, next) {
-    db.oneOrNone(`UPDATE Player SET email=$(email), name=$(name) WHERE id=${req.params.id} RETURNING id`, req.body)
-        .then(data => {
-            returnDataOr404(res, data);
-        })
-        .catch(err => {
-            next(err);
-        });
+  db.oneOrNone(
+    `UPDATE Player SET email=$(email), name=$(name) WHERE id=${req.params.id} RETURNING id`,
+    req.body
+  )
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
 function createPlayer(req, res, next) {
-    db.one(`INSERT INTO Player(email, name) VALUES ($(email), $(name)) RETURNING id`, req.body)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            next(err);
-        });
+  db.one(
+    `INSERT INTO Player(email, name) VALUES ($(email), $(name)) RETURNING id`,
+    req.body
+  )
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
 function deletePlayer(req, res, next) {
-    db.oneOrNone(`DELETE FROM Player WHERE id=${req.params.id} RETURNING id`)
-        .then(data => {
-            returnDataOr404(res, data);
-        })
-        .catch(err => {
-            next(err);
-        });
+  db.oneOrNone(`DELETE FROM Player WHERE id=${req.params.id} RETURNING id`)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
